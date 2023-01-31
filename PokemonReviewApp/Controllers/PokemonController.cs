4c10fdac5a01fdd974de54429663a55bfeb35289
Thong Quang <picothieu@gmail.com>
2023-01-31 15:35:49 +0700

    Alo123

---
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using PokemonReviewApp.Repository;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : Controller
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+        private readonly DataContext context;
+
+        public PokemonController(IPokemonRepository pokemonRepository, DataContext context)
+        {
+            _pokemonRepository = pokemonRepository;
+            this.context = context;
+        }
+
+        // GET 
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        public IActionResult GetPokemons()
+        {
+            var pokemons = _pokemonRepository.GetPokemons();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(pokemons);
+        }
+    }
+}
